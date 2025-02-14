8967ca95681e78041181fa4a9793fd8764eae67f
Stu-PID <java4first@thelewins.net>
2025-02-13 19:47:22 -0500

    New Calibration from Scratch w/ Advantage Scope
    Need to start w/ 0 values in the Rotation2d. Done through Step 17 of Setup.

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -35,10 +35,10 @@ public class DriveConstants {
       };
 
   // Zeroed rotation values for each module, see setup instructions
-  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.625);
-  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.373);
-  public static final Rotation2d backLeftZeroRotation = new Rotation2d(0.140);
-  public static final Rotation2d backRightZeroRotation = new Rotation2d(-2.224);
+  public static final Rotation2d frontLeftZeroRotation = new Rotation2d(0.018);
+  public static final Rotation2d frontRightZeroRotation = new Rotation2d(0.005);
+  public static final Rotation2d backLeftZeroRotation = new Rotation2d(-0.024);
+  public static final Rotation2d backRightZeroRotation = new Rotation2d(-0.092);
 
   // Device CAN IDs
   public static final int pigeonCanId = 10;
