f8f4beafb4023b59b7f6ea4e53d4e8362002073c
Stu-PID <java4first@thelewins.net>
2025-02-28 21:44:34 -0500

    Up the Lift Speed Factor
    0.80 so it goes faster.

--- src/main/java/frc/robot/subsystems/lift/Lift.java
+++ src/main/java/frc/robot/subsystems/lift/Lift.java
@@ -28,7 +28,6 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.config.SoftLimitConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
