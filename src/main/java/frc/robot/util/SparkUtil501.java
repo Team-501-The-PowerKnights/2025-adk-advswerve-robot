119c02019382ff3f6636f1c2ff3015e440379631
Stu-PID <java4first@thelewins.net>
2025-03-09 14:35:09 -0400

    Add 501 Version of Rev Utils
    So we can handle the Encoders.

---
+++ src/main/java/frc/robot/util/SparkUtil501.java
@@ -0,0 +1,38 @@
+/*------------------------------------------------------------------------*/
+/*- Copyright (c) Team 501 - The PowerKnights. All Rights Reserved.       */
+/*- Open Source Software - may be modified and shared by other FRC teams  */
+/*- under the terms of the Team501 license. The code must be accompanied  */
+/*- by the Team 501 - The PowerKnights license file in the root directory */
+/*- of this project.                                                      */
+/*------------------------------------------------------------------------*/
+
+/**
+ * This class contains the implementation of the <code>Lift</code> subsystem.
+ *
+ * <p>More detail ...
+ *
+ * @since 2025.0.0
+ * @author first.stu
+ * @version 2025.0.0
+ */
+package frc.robot.util;
+
+import com.revrobotics.REVLibError;
+import com.revrobotics.RelativeEncoder;
+import java.util.function.Supplier;
+
+public class SparkUtil501 extends SparkUtil {
+
+  /** Attempts to run the command until no error is produced. */
+  public static void tryUntilOk(
+      RelativeEncoder encoder, int maxAttempts, Supplier<REVLibError> command) {
+    for (int i = 0; i < maxAttempts; i++) {
+      var error = command.get();
+      if (error == REVLibError.kOk) {
+        break;
+      } else {
+        sparkStickyFault = true;
+      }
+    }
+  }
+}
