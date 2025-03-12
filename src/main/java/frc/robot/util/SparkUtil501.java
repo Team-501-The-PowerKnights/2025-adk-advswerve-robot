9066898493507867b4f43f9d03c969c1ea669b97
Stu-PID <java4first@thelewins.net>
2025-03-11 20:21:05 -0400

    Extend/Re-Do SparkUtils to Return Error
    So it can be reported - not just the fact we had one.

--- src/main/java/frc/robot/util/SparkUtil501.java
+++ src/main/java/frc/robot/util/SparkUtil501.java
@@ -19,20 +19,45 @@ package frc.robot.util;
 
 import com.revrobotics.REVLibError;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
 import java.util.function.Supplier;
 
-public class SparkUtil501 extends SparkUtil {
+public class SparkUtil501 {
+  /** Stores whether any error was has been detected by other utility methods. */
+  public static boolean sparkStickyFault = false;
+  /** */
+  public static REVLibError sparkStickyError = REVLibError.kOk;
 
   /** Attempts to run the command until no error is produced. */
-  public static void tryUntilOk(
-      RelativeEncoder encoder, int maxAttempts, Supplier<REVLibError> command) {
+  public static REVLibError tryUntilOk(
+      SparkBase spark, int maxAttempts, Supplier<REVLibError> command) {
+    REVLibError error = REVLibError.kOk;
     for (int i = 0; i < maxAttempts; i++) {
-      var error = command.get();
+      error = command.get();
       if (error == REVLibError.kOk) {
         break;
       } else {
         sparkStickyFault = true;
+        sparkStickyError = error;
       }
     }
+
+    return error;
+  }
+
+  /** Attempts to run the command until no error is produced. */
+  public static REVLibError tryUntilOk(
+      RelativeEncoder encoder, int maxAttempts, Supplier<REVLibError> command) {
+    REVLibError error = REVLibError.kOk;
+    for (int i = 0; i < maxAttempts; i++) {
+      error = command.get();
+      if (error == REVLibError.kOk) {
+        break;
+      } else {
+        sparkStickyFault = true;
+        sparkStickyError = error;
+      }
+    }
+    return error;
   }
 }
