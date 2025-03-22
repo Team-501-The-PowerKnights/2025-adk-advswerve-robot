27a9eac42999ff985e92d9ea47b71a8cbeded607
Stu-PID <java4first@thelewins.net>
2025-03-22 15:10:33 -0400

    Prep Gripper for Second Motor
    Not complete yet.

--- src/main/java/frc/robot/subsystems/gripper/Gripper.java
+++ src/main/java/frc/robot/subsystems/gripper/Gripper.java
@@ -22,7 +22,7 @@ import org.littletonrobotics.junction.Logger;
 public class Gripper extends SubsystemBase {
 
   // Hardware objects
-  private final SparkMax motor;
+  private final SparkMax leftMotor;
 
   private double currentSpeed;
 
@@ -31,18 +31,19 @@ public class Gripper extends SubsystemBase {
     // TODO - Log error on entry
 
     // Create controller
-    motor = new SparkMax(GripperConstants.gripperCanId, MotorType.kBrushless);
+    leftMotor = new SparkMax(GripperConstants.gripperCanId, MotorType.kBrushless);
     // Factory reset (but don't burn to flash)
     SparkMaxConfig config = new SparkMaxConfig();
     config
         .inverted(GripperConstants.gripperInverted)
         .idleMode(IdleMode.kBrake)
-        .voltageCompensation(12.0);
+        .smartCurrentLimit(GripperConstants.motorCurrentLimit)
+        .voltageCompensation(GripperConstants.motorVoltageComp);
     tryUntilOk(
-        motor,
+        leftMotor,
         5,
         () ->
-            motor.configure(
+            leftMotor.configure(
                 config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
 
     // Log this subsystem's status and return global
@@ -59,7 +60,7 @@ public class Gripper extends SubsystemBase {
   }
 
   private void setSpeed(double speed) {
-    motor.set(speed);
+    leftMotor.set(speed);
   }
 
   /**
@@ -79,6 +80,6 @@ public class Gripper extends SubsystemBase {
     // Update current task
     setSpeed(currentSpeed);
     Logger.recordOutput("Gripper/CurrentSpeed", currentSpeed);
-    Logger.recordOutput("Gripper/Output", motor.get());
+    Logger.recordOutput("Gripper/Output", leftMotor.get());
   }
 }
